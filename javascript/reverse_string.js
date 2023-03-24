ce093add27c393d6723c2c510427d867c16ca49a
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-23 23:19:54 -0400

    reverse string

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,15 @@
 function reverseString(str) {
-  // type your code here
+  let reverse = ""
+  for( i = (str.length -1); i > -1; i--) {
+    reverse += str[i]
+  }
+  return reverse
+  
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  console.log("Expecting: olleh")
+  console.log("=>", reverseString("hello"))
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -17,3 +23,8 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// declare a variable to store  the new string. 
+// then loop through the string backwards, storing each letter as we go through the loop
+//return the variable when the loop is finished, returning our newly reversed string
+
